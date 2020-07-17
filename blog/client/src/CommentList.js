fd4fe24c8b0ebecb84fff01a7690ab3eba3b3a48
Janne Leppänen <janne@janneleppanen.com>
2020-07-17 19:38:52 +0300

    feat(services): Add a query service

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,19 +1,6 @@
 import React from "react";
-import axios from "axios";
-import { render } from "react-dom";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = React.useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios(`http://localhost:4001/posts/${postId}/comments`);
-    setComments(res.data);
-  };
-
-  React.useEffect(() => {
-    fetchComments();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => (
     <li key={comment.id}>{comment.content}</li>
   ));
