0e8e9cc2339858e08bc5ad71b8af8435f2de0fc8
Janne Leppänen <janne@janneleppanen.com>
2020-07-18 14:09:12 +0300

    feat(services): Add moderation service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
@@ -28,6 +28,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
@@ -36,9 +37,28 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  const event = req.params;
-  console.log(req.body.type);
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  console.log("tick:" + type);
+
+  if (type === "CommentModerated") {
+    const { postId, status, id, content } = data;
+
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
   res.send({});
 });
 
